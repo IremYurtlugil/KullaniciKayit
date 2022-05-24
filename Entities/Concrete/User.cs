7f28f662bf3cedf55ae05311636b28cdb64c896e
Irem  Yurtlugil <yurtlugilirem@gmail.com>
2022-05-24 17:38:46 +0300

    .

--- Entities/Concrete/User.cs
+++ Entities/Concrete/User.cs
@@ -12,7 +12,6 @@ namespace Entities.Concrete
         public string Phone{ get; set; }
         public string Adress { get; set; }
 
-        public int? ManagerId { get; set; }
-        public Manager Manager { get; set; }
+       
     }
 }
---
+++ ViewModels/ManagerVM/LoginVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ViewModels.ManagerVM
+{
+   public class LoginVM
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} boş geçilemez")]
+        [Display(Name = " Mail Adresi", Prompt = "isimsoyisim@firmaadi.com")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} boş geçilemez")]
+        [Display(Name = "Şifre")]
+        [DataType(DataType.Password)]
+        [StringLength(8, ErrorMessage = "Şifre 8 karakterli olmalıdır.")]
+        public string Password { get; set; }
+    }
+}
